ea0a7f93cebf07d6566de661a79b1f34b6da0d42
Gautam Tata <gautamtata@gmail.com>
2019-09-01 17:34:34 -0700

    Updates

---
+++ precache-manifest.4bcd0588d4e4e3e61d7163fc8e142d59.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a648964505687cb53c02fd74d96a1e85",
+    "url": "/static/media/proofchecker.a6489645.png"
+  },
+  {
+    "revision": "4a1f6fb569b57b4bae8203df9a9edba4",
+    "url": "/static/media/pgmuseum.4a1f6fb5.png"
+  },
+  {
+    "revision": "3b17e72e03fe7c40b1e4b499700de8c2",
+    "url": "/static/media/jsmaps.3b17e72e.png"
+  },
+  {
+    "revision": "96f0b87f7212ac98a6f95f2360f3598f",
+    "url": "/static/media/gautam-profile.96f0b87f.gif"
+  },
+  {
+    "revision": "20db77d812cfcaca8b452b0adc9d6f6a",
+    "url": "/static/media/car-scan.20db77d8.gif"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dda68bdaf495fc2fb01a",
+    "url": "/static/js/main.dda68bda.chunk.js"
+  },
+  {
+    "revision": "f9ee9b89d933afbd7433",
+    "url": "/static/js/2.f9ee9b89.chunk.js"
+  },
+  {
+    "revision": "cfd47bfd27490535a1c10e470cccc826",
+    "url": "/index.html"
+  }
+];
